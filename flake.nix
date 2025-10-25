40a8e38f4c4adb6779182c6772624708d511cde9
Sami Suleiman <sulsami@protonmail.com>
2025-10-25 15:18:50 +0200

    simpler and more readable flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,36 @@
 {
-  description = "Dev environment for Angular + NestJS project";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  description = "Basic dev environment for Angular/NestJS project";
 
   outputs =
-    { self, nixpkgs }:
+    { nixpkgs, ... }:
     let
-      systems = [
-        "x86_64-linux"
+      supportedSystems = [
+        "aarch64-linux"
         "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
       ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      devShells = builtins.listToAttrs (
-        map (
-          system:
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          {
-            name = system;
-            value = {
-              default = pkgs.mkShell {
-                buildInputs = [
-                  pkgs.nodejs_22
-                  pkgs.pnpm
-                  pkgs.git
-                ];
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs_22
+              pkgs.pnpm
+              pkgs.git
+            ];
 
-                shellHook = ''
-                  echo "Welcome to the Angular dev shell 🚀"
-                  export NODE_ENV=development
-                '';
-              };
-            };
-          }
-        ) systems
+            shellHook = ''
+              export NODE_ENV=development
+            '';
+          };
+        }
       );
     };
 }
